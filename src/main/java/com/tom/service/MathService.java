6768677eea5f20888a6eac77fcfc36ab83ac17fd
armmadagus <sirawit_niyom@yahoo.com>
2021-03-21 17:02:47 +0700

    Test

--- src/main/java/com/tom/service/MathService.java
+++ src/main/java/com/tom/service/MathService.java
@@ -2,9 +2,7 @@ package com.tom.service;
 
 public class MathService {
 
-    int a;
     public int multiply(int firstNumber, int secondNumber) {
-        int b = a * 1;
         return firstNumber * secondNumber;
     }
 
