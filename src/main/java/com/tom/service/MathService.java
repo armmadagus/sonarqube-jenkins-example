48ca83b1cfc0d09910273e2348e0b619680cd2d1
armmadagus <sirawit_niyom@yahoo.com>
2021-03-29 18:54:44 +0700

    Update MathService.java

--- src/main/java/com/tom/service/MathService.java
+++ src/main/java/com/tom/service/MathService.java
@@ -6,21 +6,7 @@ public class MathService {
         return firstNumber * secondNumber;
     }
 
-    // Add Comment
-    /*
     public int subtract(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
-    
-    public int addition(int firstNumber, int secondNumber) {
-        if (firstNumber > 1)
-        {
-            System.out.println("1");
-        }
-        else if (secondNumber > 1)
-        {
-            System.out.println("2");
-        }
-        return firstNumber + secondNumber;
-    }*/
 }
